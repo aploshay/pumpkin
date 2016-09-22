02503c9dd7fa5707576b194860274f2e4405df92
Adam Ploshay <aploshay@iu.edu>
2016-09-22 10:37:25 -0400

    Revised Variations preingest to handle multi-volume, added example case

--- app/jobs/preingest_variations_job.rb
+++ app/jobs/preingest_variations_job.rb
@@ -24,9 +24,7 @@ class PreingestVariationsJob < ActiveJob::Base
       yaml_hash[:source_metadata_identifier] = @variations.xpath('//MediaObject/Label').first.content.to_s
       yaml_hash[:viewing_direction] = 'right-to-left'
       yaml_hash[:thumbnail_path] = ''
-      #
-      # check single or multi-volume
-      #
+
       # get files list
       @files = []
       @variations.xpath('//FileInfos/FileInfo').each do |file|
@@ -35,41 +33,60 @@ class PreingestVariationsJob < ActiveJob::Base
         file_hash[:checksum] = file.xpath('Checksum').first&.content.to_s
         file_hash[:mime_type] = 'image/tiff'
         file_hash[:path] = '/tmp/MS-METS/bhr9405/' + file_hash[:id]
-        file_hash[:title] = ['TITLE MISSING'] #replaced later
+        file_hash[:title] = ['TITLE MISSING'] # replaced later
         file_hash[:replaces] = 'replaces'
         file_hash[:file_opts] = {}
         @files << file_hash
       end
-      s_root = @variations.xpath('/ScoreAccessPage/RecordSet/Container/Structure/Item').first
-      yaml_hash[:structure] = {}
-      @file_index = 0
-      yaml_hash[:structure][:nodes] = structure_to_array('foo', s_root)
-      yaml_hash[:files] = @files
       yaml_hash[:thumbnail_path] = @files.first[:path]
+
+      # assign structure hash and files array
+      @file_index = 0
+      items = @variations.xpath('/ScoreAccessPage/RecordSet/Container/Structure/Item')
+      if items.size < 2
+        s_root = @variations.xpath('/ScoreAccessPage/RecordSet/Container/Structure/Item').first
+        yaml_hash[:structure] = {}
+        yaml_hash[:structure][:nodes] = structure_to_array(s_root)
+        yaml_hash[:files] = @files
+      else
+        yaml_hash[:volumes] = []
+        @file_start = 0
+        items.each do |item|
+          volume = {}
+          volume[:id] = 'ID GOES HERE'
+          volume[:title] = [item['label']]
+          volume[:nodes] = structure_to_array(item)
+          volume[:files] = @files[@file_start, @file_index - @file_start]
+          @file_start = @file_index
+          yaml_hash[:volumes] << volume
+        end
+      end
 
       yaml_hash[:source] = {}
       yaml_hash[:source][:title] = ['Variations XML']
       yaml_hash[:source][:file] = @variations_file
+
       yaml_file = @variations_file.sub(/\.xml$/, '.yml')
       File.write(yaml_file, yaml_hash.to_yaml)
       logger.info "Created YAML file #{File.basename(yaml_file)}"
     end
 
-    def structure_to_array(basename, xml_node)
+    # builds structure hash AND update file list with titles
+    def structure_to_array(xml_node)
       array = []
       xml_node.xpath('child::*').each do |child|
         c = {}
         if child.name == 'Div'
           c[:label] = child['label']
-          c[:nodes] = structure_to_array(basename, child)
-          array << c
+          c[:nodes] = structure_to_array(child)
         elsif child.name == 'Chunk'
           c[:label] = child['label']
-          @files[@file_index][:title] = [child['label']]
           c[:proxy] = @files[@file_index][:id]
+
+          @files[@file_index][:title] = [child['label']]
           @file_index += 1
-          array << c
         end
+        array << c
       end
       array
     end
