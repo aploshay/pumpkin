d8da23f240fb987fddd438cc559f549026a614e0
Adam Ploshay <aploshay@iu.edu>
2016-10-25 16:29:27 -0400

    [HPT-845] IN PROGRESS: json support; resolution for MultiVolumeWork, FileSet

--- app/controllers/purl_controller.rb
+++ app/controllers/purl_controller.rb
@@ -1,18 +1,37 @@
 # new class for imago to handle purl redirection
 class PurlController < ApplicationController
-
   def render_404
-    render :file => "/public/404.html",  :status => 404
+    render file: '/public/404.html', status: 404
   end
 
   def default
     begin
-      realid = ScannedResource.where(source_metadata_identifier: params[:id]).first.id
+      set_object
+      realid = @object.id
     rescue
-      render_404 and return
+      render_404 && return
     end
-
-    redirect_to("#{request.protocol}#{request.host_with_port}/concern/scanned_resources/#{realid}")
+    url = "#{request.protocol}#{request.host_with_port}/concern/#{@subfolder}/#{realid}"
+    respond_to do |f|
+      f.html { redirect_to url }
+      f.json { render json: url }
+    end
   end
 
+  private
+
+    OBJECT_LOOKUPS = {
+      FileSet => { match_pattern: /^\w{3}\d{4}-\d{2,3}-\d{3}/, search_attribute: :label },
+      ScannedResource => { match_pattern: /^\w{3}\d{4}$/, search_attribute: :source_metadata_identifier },
+      MultiVolumeWork => { match_pattern: /^\w{3}\d{4}$/, search_attribute: :source_metadata_identifier }
+    }
+    def set_object
+      OBJECT_LOOKUPS.each do |klass, values|
+        if params[:id].match values[:match_pattern]
+          @object = klass.where(values[:search_attribute] => params[:id]).first
+        end
+        break if @object
+      end
+      @subfolder = @object.class.to_s.pluralize.underscore
+    end
 end
--- spec/controllers/purl_controller_spec.rb
+++ spec/controllers/purl_controller_spec.rb
@@ -3,30 +3,80 @@ require 'rails_helper'
 describe PurlController do
   let(:user) { FactoryGirl.create(:user) }
   let(:scanned_resource) { FactoryGirl.create(:scanned_resource, user: user, title: ['Dummy Title'], state: 'complete', source_metadata_identifier: 'BHR9405') }
-  let(:reloaded) { scanned_resource.reload }
+  let(:multi_volume_work) { FactoryGirl.create(:multi_volume_work, user: user, title: ['Dummy Title'], state: 'complete', source_metadata_identifier: 'ABE9721') }
+  let(:file_set) { FactoryGirl.create(:file_set, user: user, label: 'BHR9405-001-001') }
 
   describe "default" do
     let(:user) { FactoryGirl.create(:admin) }
     before do
       sign_in user
+      scanned_resource
+      multi_volume_work
+      file_set
     end
-
     context "with a matching id" do
-      before(:each) do
-        get :default, id: scanned_resource.source_metadata_identifier
+      shared_examples "responses for matches" do
+        before(:each) do
+          get :default, id: id, format: format
+        end
+        context "in html" do
+          let(:format) { 'html' }
+          it "redirects to the scanned_resource page" do
+            expect(response).to redirect_to target_path
+          end
+        end
+        context "in json" do
+          let(:format) { 'json' }
+          it 'renders a json response' do
+            expect(response.body).to match target_path
+          end
+        end
       end
-      it "redirects to the scanned_resource page" do
-        expect(response).to redirect_to curation_concerns_scanned_resource_path(scanned_resource)
+      context "for a ScannedResource" do
+        let(:id) { scanned_resource.source_metadata_identifier }
+        let(:target_path) { curation_concerns_scanned_resource_path(scanned_resource) }
+        include_examples "responses for matches"
+      end
+      context "for a MultiVolumeWork" do
+        let(:id) { multi_volume_work.source_metadata_identifier }
+        let(:target_path) { curation_concerns_multi_volume_work_path(multi_volume_work) }
+        include_examples "responses for matches"
+      end
+      context "for a FileSet" do
+        let(:id) { file_set.label }
+        let(:target_path) { curation_concerns_file_set_path(file_set) }
+        include_examples "responses for matches"
       end
     end
-    context "with an unmatching id" do
+    shared_examples "returns 404 responses" do
+      context "in html" do
+        let(:format) { 'html' }
+        it "returns a 404 response" do
+          expect(response.status).to eq(404)
+          expect(response).to render_template(file: "#{Rails.root}/public/404.html")
+        end
+      end
+      context "in json" do
+        let(:format) { 'json' }
+        it "returns a 404 response" do
+          expect(response.status).to eq(404)
+          expect(response).to render_template(file: "#{Rails.root}/public/404.html")
+        end
+      end
+    end
+    context "with an invalid id" do
       before(:each) do
-        get :default, id: 'unmatching_id'
+        get :default, id: 'BHR940', format: format
       end
-      it "returns a 404 response" do
-        expect(response.status).to eq(404)
-        expect(response).to render_template(file: "#{Rails.root}/public/404.html")
+      include_examples "returns 404 responses"
+    end
+    context "with an unmatched id" do
+      before(:each) do
+        unmatched_id = scanned_resource.source_metadata_identifier
+        scanned_resource.destroy!
+        get :default, id: unmatched_id
       end
+      include_examples "returns 404 responses"
     end
   end
 end
