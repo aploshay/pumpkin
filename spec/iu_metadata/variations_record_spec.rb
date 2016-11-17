efdcc62aa9b6b26661f90119cbf91e8f3ba4a97b
Adam Ploshay <aploshay@iu.edu>
2016-11-17 18:51:45 -0500

    filling in more specs

--- spec/iu_metadata/variations_record_spec.rb
+++ spec/iu_metadata/variations_record_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe VariationsDocument do
+RSpec.describe IuMetadata::VariationsRecord do
   let(:fixture_path) { File.expand_path('../../fixtures', __FILE__) }
   let(:record1) {
     pth = File.join(fixture_path, 'variations_xml/bhr9405.xml')
-    described_class.new(pth)
+    described_class.new('file://' + pth, open(pth))
   }
   describe "parses attributes" do
     { source_metadata_identifier: 'BHR9405',
       identifier: 'http://purl.dlib.indiana.edu/iudl/variations/score/BHR9405',
-      viewing_direction: 'left-to-right',
+      viewing_hint: 'paged',
       location: 'IU Music Library',
       holding_location: 'https://libraries.indiana.edu/music',
       media: '1 score (64 p.) ; 32 cm',
