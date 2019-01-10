8908d9338d7817cfb3119751d70ce3c2c930d35a
Daniel Pierce <dlpierce@users.noreply.github.com>
2019-01-10 10:35:56 -0500

    Do not retry failed jobs, send to morgue to be retried manually. (#184)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,3 +6,9 @@ end
 Sidekiq.configure_client do |config|
   config.redis = Redis.current.client.options
 end
+
+Sidekiq.configure_server do |config|
+  config.server_middleware do |chain|
+    chain.add Sidekiq::Middleware::Server::RetryJobs, max_retries: 0
+  end
+end
