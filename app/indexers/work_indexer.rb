4014fb31478d46f6a92577db75c17aa96caadf8d
Adam Ploshay <aploshay@iu.edu>
2020-06-10 10:50:56 -0400

    [ESSI-942] Changes indexing of collection slugs to array result

--- app/indexers/work_indexer.rb
+++ app/indexers/work_indexer.rb
@@ -4,10 +4,7 @@ class WorkIndexer < CurationConcerns::WorkIndexer
   # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
   def generate_solr_document
     super.tap do |solr_doc|
-      object.member_of_collections.each do |col|
-        solr_doc[Solrizer.solr_name('member_of_collection_slugs', :symbol)] =
-          col.exhibit_id
-      end
+      solr_doc[Solrizer.solr_name('member_of_collection_slugs', :symbol)] = object.member_of_collections.map(&:exhibit_id)
 
       (PlumSchema.display_fields + [:title]).each do |field|
         objects = object.get_values(field, literal: true)
